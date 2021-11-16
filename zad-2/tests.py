c99b84b91308b319718462c6f92e372f14347e20
Rafał Majewski <goodheropl@gmail.com>
2021-11-16 01:13:13 +0100

    all tests work

--- zad-2/tests.py
+++ zad-2/tests.py
@@ -25,37 +25,37 @@ class RomanNumeralsTest(unittest.TestCase):
 	
 	def test_20_is_two_x_s(self):
 		self.assertEqual(roman(27), "XXVII")
-	@unittest.skip("Not implemented yet")
+	
 	def test_48_is_not_50_2_but_rather_40_8(self):
 		self.assertEqual(roman(48), "XLVIII")
-	@unittest.skip("Not implemented yet")
+	
 	def test_49_is_not_40_5_4_but_rather_50_10_10_1(self):
 		self.assertEqual(roman(49), "XLIX")
-	@unittest.skip("Not implemented yet")
+	
 	def test_50_is_a_single_l(self):
 		self.assertEqual(roman(59), "LIX")
-	@unittest.skip("Not implemented yet")
+	
 	def test_90_being_100_10_is_xc(self):
 		self.assertEqual(roman(93), "XCIII")
-	@unittest.skip("Not implemented yet")
+	
 	def test_100_is_a_single_c(self):
 		self.assertEqual(roman(141), "CXLI")
-	@unittest.skip("Not implemented yet")
+	
 	def test_60_being_50_10_is_lx(self):
 		self.assertEqual(roman(163), "CLXIII")
-	@unittest.skip("Not implemented yet")
+	
 	def test_400_being_500_100_is_cd(self):
 		self.assertEqual(roman(402), "CDII")
-	@unittest.skip("Not implemented yet")
+	
 	def test_500_is_a_single_d(self):
 		self.assertEqual(roman(575), "DLXXV")
-	@unittest.skip("Not implemented yet")
+	
 	def test_900_being_1000_100_is_cm(self):
 		self.assertEqual(roman(911), "CMXI")
-	@unittest.skip("Not implemented yet")
+	
 	def test_1000_is_a_single_m(self):
 		self.assertEqual(roman(1024), "MXXIV")
-	@unittest.skip("Not implemented yet")
+	
 	def test_3000_is_three_m_s(self):
 		self.assertEqual(roman(3000), "MMM")
 
