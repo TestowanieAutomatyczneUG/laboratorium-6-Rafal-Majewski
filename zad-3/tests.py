49e5533420fe471edc3cc2723d5a79c625647c44
Rafał Majewski <goodheropl@gmail.com>
2021-11-16 01:44:29 +0100

    getline(200) test

--- zad-3/tests.py
+++ zad-3/tests.py
@@ -6,7 +6,8 @@ class SongTest(unittest.TestCase):
 		self.song = Song()
 	def test_getLine_1(self):
 		self.assertEqual(self.song.getLine(1), "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.")
-
+	def test_getLine_200(self):
+		self.assertRaises(IndexError, self.song.getLine, 200)
 	def tearDown(self):
 		self.song = None
 
