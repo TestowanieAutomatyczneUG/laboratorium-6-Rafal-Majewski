22dc07841c1a1fca33d716bd79f0b9963e848c9e
Rafał Majewski <goodheropl@gmail.com>
2021-11-16 01:34:11 +0100

    first test, get line 0

--- zad-3/tests.py
+++ zad-3/tests.py
@@ -4,6 +4,8 @@ from Song import Song
 class SongTest(unittest.TestCase):
 	def setUp(self):
 		self.song = Song()
+	def test_getLine_0(self):
+		self.assertEqual(self.song.getLine(0), "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.")
 
 	def tearDown(self):
 		self.song = None
